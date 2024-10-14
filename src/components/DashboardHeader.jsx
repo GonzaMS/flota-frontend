f4f8377ac45ce734334b33c48e460223b832b35d
Cristhian Marecos <cristhianmareccos2222@gmail.com>
2024-10-14 17:21:05 -0300

    Removing comments

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -6,12 +6,10 @@ const DashboardHeader = ({ toggleSidebar }) => {
   return (
     <header className="shadow p-4 bg-gray-800 text-white">
       <div className="container mx-auto flex justify-between items-center">
-        {/* Título Dashboard */}
         <h2 className="text-lg md:text-xl lg:text-2xl font-semibold text-white">
           Dashboard
         </h2>
 
-        {/* Botón de logout */}
         <button
           className="bg-indigo-500 text-white mt-auto px-4 py-2 rounded-md hover:bg-black"
           onClick={logout}
